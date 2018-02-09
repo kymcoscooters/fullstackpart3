3b9184ca0b7bcf6f3baa515373070e878d9c1eff
Robert Holmberg <holmbrob@cs.helsinki.fi>
2018-02-09 14:51:16 +0200

    tehtävä 3.2 3.3 3.4 tehty

--- index.js
+++ index.js
@@ -25,10 +25,44 @@ let persons = [
   }
 ]
 
-app.use(bodyParser.json)
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+	res.send('<h1>Terve</h1>')
+})
 
 app.get('/api/persons', (req, res) => {
 	res.json(persons)
+})
+
+app.get('/info1', (req, res) => {
+	const amount = persons.length
+	const date = new Date()
+	res.write(`puhelinluettelossa ${amount} henkilön tiedot\n`)
+	res.write(`${new Date()}`)
+	res.end()
+})
+
+app.get('/info', (req, res) => {
+	res.send(`puhelinluettelossa ${persons.length} henkilön tiedot</br>${new Date()}`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+	const person = persons.find(person => person.id === id)
+
+	if (person) {
+		res.json(person)
+	} else {
+		res.status(404).end()
+	}
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+	persons = persons.filter(person => person.id !== id)
+
+	res.status(204).end()
 })
 
 const PORT = 3001
